bd93be79518da981293e2fe6784f1cd11d942d04
PratikAmatya <pratikamatya2@gmail.com>
2021-09-05 16:39:55 +0545

    FlatList and SectionList

--- App.js
+++ App.js
@@ -8,10 +8,12 @@
 
 import React, {useState} from 'react';
 import {
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
+  SectionList,
   Text,
   useColorScheme,
   View,
@@ -29,41 +31,129 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const [items, setItems] = useState([
-    {key: 1, item: 'item 1'},
-    {key: 2, item: 'item 2'},
-    {key: 3, item: 'item 3'},
-    {key: 4, item: 'item 4'},
-    {key: 5, item: 'item 5'},
-    {key: 6, item: 'item 6'},
+  // For Flast List
+  // const [items, setItems] = useState([
+  //   {key: '1', name: 'item 1'},
+  //   {key: '2', name: 'item 2'},
+  //   {key: '3', name: 'item 3'},
+  //   {key: '4', name: 'item 4'},
+  //   {key: '5', name: 'item 5'},
+  //   {key: '6', name: 'item 6'},
+  // ]);
+
+  // For Flat List
+  // const [items, setItems] = useState([
+  //   {name: 'item 1'},
+  //   {name: 'item 2'},
+  //   {name: 'item 3'},
+  //   {name: 'item 4'},
+  //   {name: 'item 5'},
+  //   {name: 'item 6'},
+  // ]);
+
+  const [orders, setOrders] = useState([
+    {name: 'Order 1', data: ['item 1']},
+    {name: 'Order 2', data: ['item 1', 'item 2']},
+    {name: 'Order 3', data: ['item 1', 'item 2', 'item 3']},
+    {name: 'Order 4', data: ['item 1', 'item 2', 'item 3', 'item 4']},
+    {name: 'Order 5', data: ['item 1', 'item 2', 'item 3', 'item 4', 'item 5']},
+    {
+      name: 'Order 6',
+      data: ['item 1', 'item 2', 'item 3', 'item 4', 'item 5', 'item 6'],
+    },
   ]);
 
   const [Refreshing, setRefreshing] = useState(false);
 
+  // const handleRefresh = () => {
+  //   setRefreshing(true);
+  //   setItems([
+  //     ...items,
+  //     {key: '7', name: 'item 7'},
+  //     {key: '8', name: 'item 8'},
+  //   ]);
+  //   setRefreshing(false);
+  // };
+
   const handleRefresh = () => {
     setRefreshing(true);
-    setItems([...items, {key: 7, item: 'item 7'}, {key: 8, item: 'item 8'}]);
+    setOrders([
+      {name: 'Order 7', data: ['item 1', 'item 2']},
+      {name: 'Order 8', data: ['item 1', 'item 2', 'item 3']},
+    ]);
     setRefreshing(false);
   };
+
+  // const handleFlatlistRefresh = () => {
+  //   setRefreshing(true);
+  //   setItems([...items, {name: 'item 7'}, {name: 'item 8'}]);
+  //   setRefreshing(false);
+  // };
+
   return (
-    <View>
-      <ScrollView
-        style={styles.body}
-        horizontal={false}
-        refreshControl={
-          <RefreshControl
-            refreshing={Refreshing}
-            onRefresh={handleRefresh}
-            colors={['red']}
-          />
-        }>
-        {items.map(singleitem => (
-          <View style={styles.item} key={singleitem.key}>
-            <Text style={styles.itemTitle}>{singleitem.item}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={orders}
+      renderItem={({item}) => (
+        <View>
+          <Text style={styles.itemTitle}>{item}</Text>
+        </View>
+      )}
+      renderSectionHeader={({section}) => (
+        <View style={styles.item}>
+          <Text style={styles.itemTitle}>{section.name}</Text>
+        </View>
+      )}
+      renderSectionFooter={({section}) => (
+        <View>
+          <Text>Pratik</Text>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          colors={['red', 'black', 'blue']}
+          onRefresh={handleRefresh}
+        />
+      }
+    />
+
+    // <ScrollView
+    //   style={styles.body}
+    //   horizontal={false}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={Refreshing}
+    //       onRefresh={handleRefresh}
+    //       colors={['black']}
+    //     />
+    //   }>
+    //   {items.map(singleitem => (
+    //     <View style={styles.item} key={singleitem.key}>
+    //       <Text style={styles.itemTitle}>{singleitem.name}</Text>
+    //     </View>
+    //   ))}
+    // </ScrollView>
+
+    // <FlatList
+    //   // numColumns={2}
+    //   keyExtractor={(item, index) => index.toString()}
+    //   // horizontal={true}
+    //   // inverted
+    //   data={items}
+    //   renderItem={({item}) => (
+    //     <View style={styles.item}>
+    //       <Text style={styles.itemTitle}>{item.name}</Text>
+    //     </View>
+    //   )}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={Refreshing}
+    //       onRefresh={handleFlatlistRefresh}
+    //       colors={['blue', 'red', 'black']}
+    //     />
+    //   }
+    // />
   );
 };
 
@@ -85,6 +175,7 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: 'bold',
     fontStyle: 'italic',
+    color: 'black',
   },
 });
 
